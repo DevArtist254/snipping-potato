c4f761582e0dc95ba72a6519671f7f5ef67088cc
devartist <kevinwasonga415@outlook.com>
2022-11-07 01:53:36 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {Icon} from "@iconify/react"
+import musicCover from "./assests/Alan-wa/cover.jpg"
+import song from "./assests/Alan-wa/Faded.mp3"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+  <div className="App">
+   <h1>Alan walker</h1>
+   <h2>Faded</h2>
+   <img src={musicCover} alt="cover" />
+   <audio src={song} controls />
+   <div>
+    <div>
+     <div>0.00</div>
+     <div>2.63</div>
     </div>
-  );
+    <div></div>
+   </div>
+   <div>
+    <Icon
+     icon="material-symbols:next-plan-outline-rounded"
+     width="100"
+     height="100"
+     hFlip={true}
+    />
+    <Icon icon="gg:play-pause-o" width="100" height="100" />
+    <Icon icon="gg:play-button-o" width="100" height="100" />
+    <Icon
+     icon="material-symbols:next-plan-outline-rounded"
+     width="100"
+     height="100"
+    />
+   </div>
+  </div>
+ )
 }
 
-export default App;
+export default App
