877817ec5a4bed19596fdccc36dea7987a6acfca
devartist <kevinwasonga415@outlook.com>
2022-11-09 04:26:17 +0300

    styling-c-a

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import song from "./assests/Alan-wa/Faded.mp3"
 function App() {
  return (
   <div>
-   <div>
-    <div>
+   <div className="album">
+    <div className="album-content">
      <h1>0:00</h1>
      <div className="song-cover">
       <img className="image-cover" src={musicCover} alt="cover" />
@@ -15,7 +15,7 @@ function App() {
       <div className="cic-dot"></div>
      </div>
     </div>
-    <div>
+    <div className="album-info">
      <h1>ALAN WALKER</h1>
      <h2>FADED</h2>
      <h3>ESSENTIALS</h3>
@@ -25,19 +25,19 @@ function App() {
    <div>
     <div></div>
    </div>
-   <div>
+   <div className="controls">
     <Icon
      icon="material-symbols:next-plan-outline-rounded"
-     width="100"
-     height="100"
+     width="80"
+     height="80"
      hFlip={true}
     />
-    <Icon icon="gg:play-pause-o" width="100" height="100" />
+    {/* <Icon icon="gg:play-pause-o" width="100" height="100" /> */}
     <Icon icon="gg:play-button-o" width="100" height="100" />
     <Icon
      icon="material-symbols:next-plan-outline-rounded"
-     width="100"
-     height="100"
+     width="80"
+     height="80"
     />
    </div>
   </div>
